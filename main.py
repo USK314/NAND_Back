ea100ed75b91b40eef4851c47ad67940ed54bfe5
USK314 <usk31101021@gmail.com>
2022-02-18 14:22:18 +0900

    originやmiddlewareを設定した

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import json
 from fastapi import FastAPI, Form, status
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import crud
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5501"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # ルート
 @app.get("/")
